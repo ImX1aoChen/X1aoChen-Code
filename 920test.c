a0830047f299259f31dc26b5aef62e278ebd3d9d
ImX1aoChen <2507092133@qq.com>
2024-09-21 16:36:34 +0800

    Add files via upload

---
+++ 920test.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	printf("123123\n");
+//	printf("123123\n");
+//	printf("123123\n");
+//	printf("123123\n");
+//	printf("123123\n");
+//	printf("123123\n");
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//
+//	scanf("%d %d", &num1, &num2);
+//
+//	int sum = num1 + num2;
+//	printf("%d\n",sum);
+//
+//
+//	return 0;
+//}
+
+
+//extern; int a;
+//
+//int main()
+//{
+//	
+//	printf("a=%d\n", a);
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	const int a = 0;
+//	
+//
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+int main()
+{
+	enum color
+	{
+		RED,
+		BLUE,
+		GREEN
+	};
+	int num = 10;
+	enum color R = RED;
+
+	printf("%d\n", R);
+
+
+	return 0;
+}
