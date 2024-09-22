6c300e9f85ce687589bfadd3f8766e8e4dfee89c
ImX1aoChen <2507092133@qq.com>
2024-09-22 11:42:36 +0800

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+//int main()
+//{
+	//char arr1[] = "abcdef";
+	//char arr2[] = { 'a','b','c','d','e','f'};
+
+	//printf("%d\n", strlen(arr1));
+	//printf("%d\n", strlen(arr2));
+
+	//int len = strlen("abcdef");
+	//printf("%d\n", len);
+
+	//printf("%s\n", arr1);
+	//printf("%s\n", arr2);
+//	return 0;
+//	
+//}
+
